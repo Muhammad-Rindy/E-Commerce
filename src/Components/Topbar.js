0d21325d73409181b9c69f27b60a59a4533adb50
muhammad rindy <muhammadrindy2705@gmail.com>
2022-08-05 17:04:34 +0700

    first commit

---
+++ src/Components/Topbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { IoIosLogIn } from "react-icons/io";
+
+function Topbar() {
+  return (
+    <Navbar bg="light" expand="lg" className="mb-3">
+      <Container fluid>
+        <Navbar.Brand href="#">Rid-eer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Product</Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>
+            <Nav.Link href="#action2">Contact</Nav.Link>
+          </Nav>
+          <Button variant="outline-dark m-2">
+            <a href="#">
+              <IoIosLogIn />
+            </a>
+            Login
+          </Button>
+          <Button variant="outline-dark m-2">
+            <a href="#">
+              <IoIosLogIn />
+            </a>
+            Register
+          </Button>
+          <Button variant="outline-dark m-2">
+            <a href="#">
+              <IoIosLogIn />
+            </a>
+            Chart (0)
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Topbar;
